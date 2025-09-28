6dc5efa0d1131f59f5910357ee3f1ba49a6ad371
Deep Prajapati <dvprajapati7212@gmail.com>
2025-09-28 18:29:00 +0530

    feat: implemented view notes functionality

--- src/pages/ViewNote.jsx
+++ src/pages/ViewNote.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import toast from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router';
 
 export default function ViewNote() {
+  const { id } = useParams();
+  
+  const allNotes = useSelector((state) => state.notes.notes);
+  
+  const note = allNotes.find((n) => n._id === id);
+  
   return (
-    <div>ViewNote</div>
+    <div>
+      <h4>{note.title}</h4>
+      <p>{ note.content}</p>
+    </div>
   )
 }
