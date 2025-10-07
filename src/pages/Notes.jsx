749fb8ad3d9f8a987d9a0ccebf92d5c98d468303
Deep Prajapati <dvprajapati7212@gmail.com>
2025-10-07 21:40:28 +0530

    feat: implemented share functionality in Notes page

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -96,7 +96,34 @@ export default function Notes() {
                     Copy
                   </button>
 
-                  <button className="px-3 py-1.5 bg-yellow-100 text-yellow-700 text-xs font-medium rounded-md cursor-pointer hover:bg-yellow-200 transition-colors duration-200 flex items-center gap-1.5"> <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <button onClick={async () => {
+            if (navigator.share) {
+              try {
+                await navigator.share({
+                  title: note?.title || "Untitled Note",
+                  text: note?.content,
+                  url:window.location.href
+                })
+                const shareLink =`${window.location.origin}/notes/${note._id}`;
+                console.log(shareLink);
+                
+                  await navigator.clipboard.writeText(shareLink);
+                  toast.success('Link copied to clipboard!')
+                toast.success('Notes shared successfully')
+              } catch (error) {
+                if (error.name !== 'AbortError') {
+                  console.log('Error sharing', error);
+                  const shareLink = `${window.location.origin}/notes/${note._id}`;
+                 await navigator.clipboard.writeText(shareLink);
+                  toast.success('Link copied to clipboard!')
+                  
+                } else {
+                  const shareLink = `${window.location.origin}/notes/${note._id}`;
+                 await navigator.clipboard.writeText(shareLink);
+                  toast.success('Link copied to clipboard!')
+                }
+              }            }
+          }} className="px-3 py-1.5 bg-yellow-100 text-yellow-700 text-xs font-medium rounded-md cursor-pointer hover:bg-yellow-200 transition-colors duration-200 flex items-center gap-1.5"> <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.367 2.684 3 3 0 00-5.367-2.684z" />
                 </svg> Share</button>
                 </div>
